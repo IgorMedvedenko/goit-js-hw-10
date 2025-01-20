b5c6bd448162005b32730966f5a4bc2b9001d8aa
IgorMedvedenko <noveciti0@gmail.com>
2025-01-20 17:12:59 +0200

    finishGenerator

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+import iziToast from 'izitoast';
+import 'iziToast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.delay.value);
+  const state = event.target.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Promise Fulfilled',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Promise Rejected',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
